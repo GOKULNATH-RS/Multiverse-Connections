97654a983731d455719ec4359d86a5f6854e132f
gokulnathpeace@gmail.com <gokulnathpeace@gmail.com>
2023-09-07 17:18:44 +0530

    tracking adjusted

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       </div>
       <section className="bg-HeroBg bg-cover bg-center h-[100vh] w-[100vw] font-aquire text-white flex-col flex-center gap-20">
         <div className="flex flex-col items-center justify-center">
-          <p className="max-md:tracking-[15px] tracking-[25px] font-aquireLight max-md:text-sml text-md">
+          <p className="tracking-[15px] lg:tracking-[25px] font-aquireLight max-md:text-sml text-md">
             U N L O C K T H E
           </p>
           <p className="text-5xl lg:text-8xl ">Multiverse</p>
